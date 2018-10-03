b28d5d8107273e4a1874b8ca6db22331ddb00a84
duongtdn <duongtdn@gmail.com>
2018-10-03 13:15:30 +0000

    update to newest version of account-base

--- api/aws.js
+++ api/aws.js
@@ -2,36 +2,54 @@
 
 require('dotenv').config()
 
-const awsServerlessExpress = require('aws-serverless-express')
-const app = require('@stormgle/user-services')
-const dynamodb = require('@stormgle/userdb-dynamodb')
+/* create api */  
+const api = require('@stormgle/account-base')
 
-const DB = {
-  HOST: process.env.DB_HOST,
-  PORT: process.env.DB_PORT
+const DatabaseAbstractor = require("database-abstractor")
+const userdb = new DatabaseAbstractor()
+const dynamodb = require('@stormgle/userdb-dynamodb-driver')
+
+const region = 'ap-southeast-1';
+const endpoint = 'dynamodb.ap-southeast-1.amazonaws.com';
+
+userdb.use(dynamodb({ region, endpoint }));
+
+const forgotPassword = {
+  onSuccess: (token) => console.log(token.token),
+  onFailure: (err) => console.log(err)
+};
+
+const form = {
+  title: 'Auth-O', 
+  body:'Auth-O Service',
+  endPoint: `https://auth-o.com/auth/reset_password`,
+  redirect: {
+    success: `https://auth-o.com/`
+  }
+};
+
+const reset = {
+  title: 'Auth-O', 
+  service: 'Expiup',
+  redirect: `https://auth-o.com/`
 }
 
-const dbDriver = dynamodb({ 
-  region : 'us-west-2', 
-  endpoint : `${DB.HOST}:${DB.PORT}`
-});
+api.useDatabase({ userdb })
+   .generateFunctions({forgotPassword, form, reset});
 
+
+/* create express app from api */  
+const express = require('express')
+const cors = require('cors')
+
+const app = express();
 app
-  .useDbDriver(dbDriver)
+  .use(cors())
+  .use('/', api)
 
-const funcs = [
-  'post/auth/signup',
-  'post/auth/login',
-  'post/me/update_profile',
-  'post/me/update_password',
-  'post/users/update',
-  'get/users/:username'
-]
-funcs.forEach( (func) => {
-  const { method, uri, includePath } = app.parseApi(func);
-  app.createFunction(method, uri, require(`@stormgle/user-services/api/${includePath}`))
-})  
 
+/* wrap into lambda */  
+const awsServerlessExpress = require('aws-serverless-express')
 const server = awsServerlessExpress.createServer(app)
 exports.handler = (event, context) => {
   awsServerlessExpress.proxy(server, event, context)
